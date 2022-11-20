db13108d8b3362e083507a90d0a95fac8962fd9a
ashraf-frotan <79718476+ashraf-frotan@users.noreply.github.com>
2022-11-20 22:41:52 +0430

    tailwindcss added, components created

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,99 @@
 <template>
-  <div>this is product details {{ id }}</div>
+  <div>
+    <ProductDetails :product="product" />
+  </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
 definePageMeta({
   layout: "products",
+});
+
+var products = [
+  {
+    id: 1,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description: "",
+  },
+  {
+    id: 2,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 4,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 3,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 5,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 6,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 7,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 8,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 9,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+];
+var product = {
+  id: 1,
+  title: "Warm winter coat",
+  price: 12.4,
+  image: "1.jpg",
+  description: "",
+};
+products.forEach((p) => {
+  if (p.id == id) {
+    product = p;
+  }
 });
 </script>
 
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,18 +1,90 @@
 <template>
   <div>
-    <h2>Products Page</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam ratione
-      asperiores odio quis exercitationem ipsum molestiae, iure rem perferendis
-      vitae soluta, id magnam eaque modi atque expedita eum in doloribus!
-    </p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products" :key="p.id">
+        <ProductCard :product="p" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
 definePageMeta({
   layout: "products",
 });
+var products = [
+  {
+    id: 1,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description: "",
+  },
+  {
+    id: 2,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 4,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 3,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 5,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 6,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 7,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 8,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+  {
+    id: 9,
+    title: "Warm winter coat",
+    price: 12.4,
+    image: "1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quia atque dignissimos nisi labore consequatur numquam aspernatur consequuntur iusto suscipit, impedit quam at molestias, nemo eos ad culpa, maiores ullam!",
+  },
+];
 </script>
 
 <style>
